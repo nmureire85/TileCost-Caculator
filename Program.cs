055c8667af6f78395e2e58ea83b74cb02f41a05d
nmureire85 <nmureire@gmail.com>
2026-06-18 23:09:39 +0100

    TileCost calculator completed task and tidy up

--- Program.cs
+++ Program.cs
@@ -4,29 +4,84 @@ class Program
 {
     static void Main(string[] args)
     {
-        int width;
-        int height;
-        int costPerUnitFlooring;
-        double totalCostOfFlooring;
-        int areaOfFlooring;
-        
-        Console.WriteLine("Welcome to the Tile Cost Caculator!. Brought to you by Nelson Construction LTD\nBefore we proceed, let us collect some input");
+        Console.WriteLine(
+            "Welcome to the Tile Cost Caculator!. Brought to you by Nelson Construction LTD\nBefore we proceed, let us collect some input");
+
         Console.Write("Enter the width in ft: ");
-        width = int.Parse(Console.ReadLine());
-        
+        int width = int.Parse(Console.ReadLine());
+
         Console.Write("Enter the height in ft: ");
-        height = int.Parse(Console.ReadLine());
-            
+        int height = int.Parse(Console.ReadLine());
+
         Console.Write("Enter the cost per unit in pounds: ");
-        costPerUnitFlooring = int.Parse(Console.ReadLine());
-        
-        areaOfFlooring = width * height;
-        Console.WriteLine($"Area of flooring is {areaOfFlooring} ");
-        
-        totalCostOfFlooring = areaOfFlooring  * costPerUnitFlooring;    
-        Console.Write($"The total cost of flooring is {totalCostOfFlooring} pounds.");
-        
-        
-        
+        int costPerUnitFlooring = int.Parse(Console.ReadLine());
+
+        int areaOfFlooring = width * height;
+        Console.WriteLine($"Area of flooring is {areaOfFlooring} sq ft ");
+
+        double totalCostOfFlooring = areaOfFlooring * costPerUnitFlooring;
+        Console.Write($"The total cost of flooring is: ${totalCostOfFlooring}.");
+
+        Console.Write("\nEnter shape of non-rectangular room: ");
+        string shapeOfNonRectangularRoom = Console.ReadLine();
+
+        const int FLOORING_PER_HOUR = 20;
+        const double COST_OF_FLOORING_PER_HOUR = 86.00;
+        const double PI = 3.14159;
+
+        if (shapeOfNonRectangularRoom == "triangle")
+        {
+            Console.Write("Enter Base of triangle: ");
+            int baseofTriangle = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter height of triangle: ");
+            int heightOfTriangle = int.Parse(Console.ReadLine());
+
+            double costPerUnitOfTriangularRoom = COST_OF_FLOORING_PER_HOUR / FLOORING_PER_HOUR;
+
+            double areaOfTriangle = (baseofTriangle * heightOfTriangle) / 2;
+
+            double totalLabourCostForTriangularRoom = areaOfTriangle * costPerUnitOfTriangularRoom;
+
+            Console.Write($"The total labour cost for triangular room is: ${totalLabourCostForTriangularRoom}");
+        }
+
+        if (shapeOfNonRectangularRoom == "circle")
+        {
+            Console.Write("Radius of cirlcle: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            double costPerUnitOfCircularRoom = COST_OF_FLOORING_PER_HOUR / FLOORING_PER_HOUR;
+
+            double areaOfCircle = PI * radius * radius;
+
+            double totalLabourCostForCircleRoom = areaOfCircle * costPerUnitOfCircularRoom;
+
+            Console.Write($"The total labour cost for circular room is: ${totalLabourCostForCircleRoom}");
+        }
+
+        if (shapeOfNonRectangularRoom == "square")
+
+        {
+            Console.Write("Enter side length of square: ");
+            int side1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second side length of square: ");
+            int side2 = int.Parse(Console.ReadLine());
+
+            double costPerUnitOfSquareRoom = COST_OF_FLOORING_PER_HOUR / FLOORING_PER_HOUR;
+
+            double areaOfSquare = side1 * side2;
+
+            double totalLabourCostForSquareRoom = areaOfSquare * costPerUnitOfSquareRoom;
+
+            Console.Write($"The total labour cost for square room is: ${totalLabourCostForSquareRoom}");
+        }
+
+        if (shapeOfNonRectangularRoom != "square" && shapeOfNonRectangularRoom != "circle" &&
+            shapeOfNonRectangularRoom != "triangle")
+        {
+            Console.Write($"The shape entered  is not supported !!!!. Please try again.");
+        }
     }
 }
